7e16e5003658f8b5b6c123086258930e71d5a0b9
Timothy Gonzalez <timg456789@yahoo.com>
2016-03-24 22:46:41 -0400

    I was able to remove a line of code that was important, but a test didn't fail. I guess it wasn't so slick to move test code into production, but I did build it as necessary. I think there is just a loop hole and the three rules of TDD are too simple.

--- test.js
+++ test.js
@@ -47,9 +47,24 @@ test('seek out the month with three paychecks from 03-24-2016', function(t) {
 });
 
 test('seek out the month with three paychecks from 04-29-2016', function(t) {
-    t.plan(1);
+    t.plan(10);
     var startDate = new Date(2016, 3, 29);
-    var triple = calc.getThreePayCheckMonth(startDate.getTime());
+    var tripleFirst = calc.getThreePayCheckMonth(startDate.getTime());
+    var tripleSecond = calc.getNextBiweeklyPayDateFrom(tripleFirst.getTime());
+    var tripleThird = calc.getNextBiweeklyPayDateFrom(tripleSecond.getTime());
 
-    t.ok(calc.monthHasThreePayChecks(triple.getTime()));
+    t.ok(calc.monthHasThreePayChecks(tripleFirst.getTime()));
+
+    t.equal(tripleFirst.getFullYear(), 2016);
+    t.equal(tripleFirst.getMonth(), 8);
+    t.equal(tripleFirst.getDate(), 2);
+
+    t.equal(tripleSecond.getFullYear(), 2016);
+    t.equal(tripleSecond.getMonth(), 8);
+    t.equal(tripleSecond.getDate(), 16);
+
+    t.equal(tripleThird.getFullYear(), 2016);
+    t.equal(tripleThird.getMonth(), 8);
+    t.equal(tripleThird.getDate(), 30);
+
 });
