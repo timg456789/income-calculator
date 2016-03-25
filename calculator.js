6bb82133a8b271e9dbc53890511ab41b316e61c9
Timothy Gonzalez <timg456789@yahoo.com>
2016-03-25 00:50:20 -0400

    Check for dates more clearly.

--- calculator.js
+++ calculator.js
@@ -36,18 +36,23 @@ exports.monthHasThreePayChecks = function (time) {
 
     var hasThree = false;
 
-    var triple = new Date(time);
+    var first = new Date(time);
+    var second = new Date(time);
+    var third = new Date(time);
 
-    if (triple.getDay() === FRIDAY) {
+    var month = first.getMonth();
 
-        var triplesMonth = triple.getMonth();
-        triple.setDate(triple.getDate() + BIWEEKLY_INTERVAL);
+    second.setDate(first.getDate() + BIWEEKLY_INTERVAL);
+    // This is going to fail on a leap year after looking through the calendar.
+    // I knew it was too simple to start, but I didn't figure it out now until
+    // looking at both the calendar and the code.
+    third.setDate(first.getDate() + BIWEEKLY_INTERVAL * 2);
 
-        if (isDayOfMonth(triple, triplesMonth, FRIDAY)) {
+    if (first.getDay() === FRIDAY) {
 
-            triple.setDate(triple.getDate() + BIWEEKLY_INTERVAL);
+        if (isDayOfMonth(second, month, FRIDAY)) {
 
-            if (isDayOfMonth(triple, triplesMonth, FRIDAY)) {
+            if (isDayOfMonth(third, month, FRIDAY)) {
                 hasThree = true;
             }
         }
