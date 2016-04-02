9ada209bc17021ff900fb57fdc6d9173c32ccd48
Timothy Gonzalez <timg456789@yahoo.com>
2016-04-02 18:35:31 -0400

    this should be a good one.

--- expense-test.js
+++ expense-test.js
@@ -1,33 +1,50 @@
 var test = require('tape');
 
-test('calculate expenses', function(t) {
+var car = {
+    amount: 125
+};
+
+var rent = {
+    amount: 550
+};
+
+var carInsurance = {
+    amount: 335.35
+};
+
+var utilities = {
+    amount: 165
+};
+
+var phone = {
+    amount: 100
+};
+
+var monthlyExpenses = [rent, carInsurance, utilities, phone];
+var weeklyExpenses = [car];
+
+test('calculate recurring expenses for april 2016', function(t) {
     t.plan(1);
 
-    var car = {
-        amount: 125
-    };
-
-    var rent = {
-        amount: 550
-    };
-
-    var carInsurance = {
-        amount: 335.35
-    };
-
-    var utilities = {
-        amount: 165
-    };
-
-    var phone = {
-        amount: 100
-    };
-
-    var monthlyExpenses = [rent, carInsurance, utilities, phone];
-    var weeklyExpenses = [car];
     var recurringExpensesApril2016 = 1775.35;
 
-    t.equal(getExpenses(monthlyExpenses, weeklyExpenses), recurringExpensesApril2016, 'expenses for april 2016 are: ' + recurringExpensesApril2016);
+    t.equal(
+        getExpenses(monthlyExpenses, weeklyExpenses),
+        recurringExpensesApril2016,
+        'expenses for april 2016 are: ' + recurringExpensesApril2016
+    );
+});
+
+test('calculate recurring expenses for may 2016', function(t) {
+    t.plan(1);
+
+    var expected = 1650.35;
+
+    t.equal(
+        getExpenses(monthlyExpenses, weeklyExpenses),
+        expected,
+        'expenses for may 2016 are: ' + expected
+    );
 });
 
 test('there are 5 fridays in april 2016', function(t) {
