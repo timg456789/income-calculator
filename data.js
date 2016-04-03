ec54c400f5c54d91d2d50beb235990172db456f8
Timothy Gonzalez <timg456789@yahoo.com>
2016-04-03 08:19:44 -0400

    Move the data out of the tests.

---
+++ data.js
@@ -0,0 +1,26 @@
+
+exports.monthlyExpenses = [
+    {
+        name: "rent",
+        amount: 550
+    },
+    {
+        name: "carInsurance",
+        amount: 335.35
+    },
+    {
+        name: "utilities",
+        amount: 165
+    },
+    {
+        name: "phone",
+        amount: 100
+    }
+];
+
+exports.weeklyExpenses = [
+    {
+        name: "car",
+        amount: 125
+    }
+];
--- expense-test.js
+++ expense-test.js
@@ -1,37 +1,15 @@
 var test = require('tape');
 var calc = require('./calculator');
 var cal = require('./calendar');
-
-var car = {
-    amount: 125
-};
-
-var rent = {
-    amount: 550
-};
-
-var carInsurance = {
-    amount: 335.35
-};
-
-var utilities = {
-    amount: 165
-};
-
-var phone = {
-    amount: 100
-};
-
-var monthlyExpenses = [rent, carInsurance, utilities, phone];
-var weeklyExpenses = [car];
+var data = require('./data');
 
 test('calculate recurring expenses for april 2016', function(t) {
     t.plan(1);
 
     var recurringExpensesApril2016 = 1775.35;
     var actual = calc.getExpenses(
-        monthlyExpenses,
-        weeklyExpenses,
+        data.monthlyExpenses,
+        data.weeklyExpenses,
         cal.FRIDAY,
         cal.APRIL,
         2016
@@ -49,7 +27,8 @@ test('calculate recurring expenses for may 2016', function(t) {
 
     var expected = 1650.35;
     var actual = calc.getExpenses(
-        monthlyExpenses, weeklyExpenses,
+        data.monthlyExpenses,
+        data.weeklyExpenses,
         cal.FRIDAY,
         cal.MAY,
         2016);
