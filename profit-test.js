9083f19beaee00faecb5947b8e3cc4e1857ccd90
Timothy Gonzalez <timg456789@yahoo.com>
2016-04-04 19:40:09 -0400

    next test.

--- profit-test.js
+++ profit-test.js
@@ -3,26 +3,33 @@ var calc = require('./calculator');
 var data = require('./data');
 
 test('profits for april 2016', function(t) {
-    t.plan(3);
+    t.plan(2);
 
     var oneTimeExpense = [{ amount: 320000 }];
-    var savings =  [
-        { amount: 126341 },
-        { amount: 821 }
-    ];
 
     var netIncome = calc.getNetIncome(
         data.aprilExpensesConfig,
         data.aprilIncomeConfig(),
-        oneTimeExpense,
-        savings
+        oneTimeExpense
     );
 
     var expectedProfits = data.recurringIncomeApril2016 - data.recurringExpensesApril2016;
     expectedProfits = expectedProfits - oneTimeExpense[0].amount;
     t.equal(expectedProfits, -17535, 'profit or loss for month');
     t.equal(netIncome, expectedProfits, 'profits for april 2016: ' + netIncome);
+});
 
-    var total = expectedProfits + savings[0].amount + savings[1].amount;
-    t.equal(total, 109627); // cents
+test('projected net assets starting march 1 2016', function(t) {
+    t.plan(1);
+
+    var savings =  [
+        { amount: 126341 },
+        { amount: 821 }
+    ];
+    var projectedMonthNetIncome = -17535;
+    var expect = projectedMonthNetIncome + savings[0].amount + savings[1].amount;
+    t.equal(expect, 109627);
+
+    var netAssets = calc.getNetAssets(projectedMonthNetIncome, savings);
+    t.equal(netAssets, expect);
 });
