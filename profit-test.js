85939cf98e05799471a39c4ca6adbe8d3055beb2
Timothy Gonzalez <timg456789@yahoo.com>
2016-04-04 19:29:21 -0400

    I'm eventually going to need to make everything transactional. Debugging is getting difficult. I don't know what's right anymore, my math on a calculator or the program.

--- profit-test.js
+++ profit-test.js
@@ -1,21 +1,28 @@
 var test = require('tape');
-var cal = require('./calendar');
 var calc = require('./calculator');
 var data = require('./data');
 
 test('profits for april 2016', function(t) {
-    t.plan(2);
+    t.plan(3);
 
-    var oneTimeExpense = 3200 * 100;
+    var oneTimeExpense = [{ amount: 320000 }];
+    var savings =  [
+        { amount: 126341 },
+        { amount: 821 }
+    ];
 
     var netIncome = calc.getNetIncome(
         data.aprilExpensesConfig,
         data.aprilIncomeConfig(),
-        [{ amount: oneTimeExpense }]
+        oneTimeExpense,
+        savings
     );
 
     var expectedProfits = data.recurringIncomeApril2016 - data.recurringExpensesApril2016;
-    expectedProfits = expectedProfits - oneTimeExpense;
-    t.equal(expectedProfits, -17535);
+    expectedProfits = expectedProfits - oneTimeExpense[0].amount;
+    t.equal(expectedProfits, -17535, 'profit or loss for month');
     t.equal(netIncome, expectedProfits, 'profits for april 2016: ' + netIncome);
+
+    var total = expectedProfits + savings[0].amount + savings[1].amount;
+    t.equal(total, 109627); // cents
 });
