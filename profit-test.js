440c1d8efd9e9ced073cef228d04e6450e8ff8fb
Timothy Gonzalez <timg456789@yahoo.com>
2016-04-06 21:06:15 -0400

    consolidate the preparatory test.

--- profit-test.js
+++ profit-test.js
@@ -4,7 +4,7 @@ var calc = require('./calculator');
 var data = require('./data');
 
 test('profits for april 2016', function(t) {
-    t.plan(2);
+    t.plan(3);
 
     var oneTimeExpense = [{ amount: 320000 }];
 
@@ -25,24 +25,17 @@ test('profits for april 2016', function(t) {
         }
     );
 
-    var expectedProfits = data.recurringIncomeApril2016 - data.recurringExpensesApril2016;
-    expectedProfits = expectedProfits - oneTimeExpense[0].amount;
-    t.equal(expectedProfits, -17535, 'profit or loss for month');
-
-    console.log(netIncome);
-
-    t.equal(netIncome, expectedProfits, 'profits for april 2016: ' + netIncome);
-
-});
-
-test('projected net assets starting march 1 2016', function(t) {
-    t.plan(1);
+    var expectedNetIncome = data.recurringIncomeApril2016 - data.recurringExpensesApril2016;
+    expectedNetIncome = expectedNetIncome - oneTimeExpense[0].amount;
+    t.equal(expectedNetIncome, -17535, 'profit or loss for month');
+    t.equal(netIncome, expectedNetIncome, 'profits for april 2016: ' + netIncome);
 
     var savings =  [
         { amount: 126341 },
         { amount: 821 }
     ];
-    var projectedMonthNetIncome = -17535;
-    var expect = projectedMonthNetIncome + savings[0].amount + savings[1].amount;
+
+    var expect = netIncome + savings[0].amount + savings[1].amount;
     t.equal(expect, 109627);
 });
+
