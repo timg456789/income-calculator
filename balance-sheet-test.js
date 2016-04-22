4ce629b44558904d9281a487e1303116d4abc9d8
Timothy Gonzalez <timg456789@yahoo.com>
2016-04-22 03:39:48 -0400

    books for the night: beer, mcdonalds, salems - gyro and oysters, 450 taxes, 350 transfer from checkings to savings, 82 walmart groceries, 23.60 gas, 4.03 pdq, 10 sunpass, 70 walmart:pokemon cards for jon and tyler they were only two dollars a pack these certain ones for some reason normally they are 4.18 so i bought all they had 24 packs and 10 face cleaning wipes, 10 legos, 100 petty cash, 24.99 monthly subscription.

--- balance-sheet-test.js
+++ balance-sheet-test.js
@@ -39,4 +39,41 @@ test('cash balance at 09:11PM EDT 04/18/2016', function(t) {
     t.equal(790.36, available / denom, ' expected cash available until next paycheck: ' + available / denom);
 });
 
+test('cash balance at 03:32AM EDT 04/22/2016', function(t) {
+    t.plan(3);
+
+    var denom = 100;
+
+    var assets = [35821, 138874, 51696];
+    var cash = balanceSheet.sum(assets);
+    var sumDollars = (cash / denom);
+
+    t.equal(sumDollars, 2263.91, 'cash: ' + sumDollars);
+
+    var paid = [
+        55000, // rent
+        14000, // utilities - electric, water, garbage, cable, internet
+	45800 // taxes
+    ];
+
+    var sumPaid = balanceSheet.sum(paid);
+    var paidDollars = (sumPaid / denom);
+
+    console.log('paid: ' + paidDollars);
+
+    var expenses = [
+        33535, // car insurance
+        10000, // phone
+        143200, // dentist
+    ];
+
+    var owed = balanceSheet.sum(expenses);
+    var expenseDollars = owed / denom;
+
+    t.equal(expenseDollars, 1867.35, 'cash owed: ' + expenseDollars);
+
+    var available = cash - owed;
+
+    t.equal(396.56, available / denom, ' expected cash available until next paycheck: ' + available / denom);
+});
 
