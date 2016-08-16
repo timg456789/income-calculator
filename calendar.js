35cef88360da75dc551a5f746a735e8b10d5c364
Timothy Gonzalez <timg456789@yahoo.com>
2016-08-16 00:03:39 -0400

    push back the first pay date for yearly projections in 2016.

--- calendar.js
+++ calendar.js
@@ -3,7 +3,7 @@ exports.APRIL = 3;
 exports.MAY = 4;
 exports.SEPTEMBER = 8;
 
-exports.BIWEEKLY_PAY_START_DATE = new Date(2016, exports.MARCH, 18);
+exports.BIWEEKLY_PAY_START_DATE = new Date(2015, 11, 25);
 exports.BIWEEKLY_INTERVAL = 14;
 exports.FRIDAY = 5;
 
