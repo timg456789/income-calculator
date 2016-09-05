4bb7ce7b3a4f6afdb106855c1cc5e16b11675613
Timothy Gonzalez <timg456789@yahoo.com>
2016-09-05 13:42:07 +0000

    update gitignore after moving personal budget to just a config file for the UI. Document the purpose of budget parser in more detail.

--- src/budget-parser.js
+++ src/budget-parser.js
@@ -1,3 +1,7 @@
+// Used to detect and convert iso strings to date objects.
+// Allows a JS object with a date property to be serialized
+// and deserialized. Normally this breaks, since the date is
+// serialized to a string, but never deserialized to a date.
 exports.parse = function (budget) {
     return JSON.parse(budget, function (k,v) {
 
