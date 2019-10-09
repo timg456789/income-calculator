38be4f59aaa958d4453273072b31826dc1b0fb2f
Timothy Gonzalez <timg456789@yahoo.com>
2019-10-08 23:31:53 -0400

    add dotted underline for tangibles.

--- src/views/balance-sheet/property-plant-and-equipment-view-model.js
+++ src/views/balance-sheet/property-plant-and-equipment-view-model.js
@@ -11,16 +11,20 @@ function PropertyPlantAndEquipmentViewModel() {
     };
     this.getHeaderView = function () {
         return $(`<div class="row table-header-row">
-              <div class="col-xs-3">Value</div>
               <div class="col-xs-9">Name</div>
+              <div class="col-xs-3">Value</div>
           </div>`);
     };
     this.getReadOnlyView = function (amount, name) {
         'use strict';
-        let view = $(`<div class="asset-item row transaction-input-view">
-                    <div class="col-xs-3 text-right vertical-align amount-description-column">${Util.format(amount)}</div>
-                    <div class="col-xs-9 vertical-align amount-description-column">${name}</div>
-                  </div>
+        let view = $(`
+            <div class="property-plant-and-equipment-row row transaction-input-view">
+                    <div class="col-xs-9 vertical-align amount-description-column">
+                        <div class="dotted-underline">${name}</div></div>
+                    <div class="col-xs-3 text-right vertical-align amount-description-column">
+                        <div class="dotted-underline">${Util.format(amount)}</div>
+                    </div>
+            </div>
         `);
         let viewContainer = $('<div></div>');
         viewContainer.append(view);
@@ -28,14 +32,14 @@ function PropertyPlantAndEquipmentViewModel() {
     };
     this.getView = function () {
         let view = $(`<div class="asset-item row transaction-input-view">
+                    <div class="col-xs-9">
+                        <input class="name form-control text-right" type="text" />
+                    </div>
                     <div class="col-xs-3">
                         <div class="input-group">
                             <div class="input-group-addon ">$</div>
                             <input class="amount form-control text-right" type="text" placeholder="0.00"/>
                         </div>
-                    </div>
-                    <div class="col-xs-9">
-                        <input class="name form-control text-right" type="text" />
                     </div>
                   </div>
         `);
